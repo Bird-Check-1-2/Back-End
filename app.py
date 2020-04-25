39cb3fbcbe188018e43c399c4246f5d13a7e18d2
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-25 10:01:35 -0400

    all endpoints workin locally

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+import pandas as pd
+
 from flask import Flask, jsonify, request
 from joblib import load
 
 app = Flask(__name__)
+
+encoder = load('utils/cat_boost.joblib')
+model = load('utils/rf.joblib')
 
 birds = load('utils/birds_list.joblib')
 seasons = load('utils/seasons_list.joblib')
 states = load('utils/states.joblib')
 
 state_counties = load('utils/state_counties.joblib')
+cs2r = load('utils/counties_to_regions.joblib')
+
+labels = {0: "Common", 1:"Uncommon", 2:"Rare"}
 
 @app.route('/api/birds', methods=['GET'])
 def get_birds():
@@ -42,6 +50,34 @@ def get_county_from_state():
         'counties': sorted(counties)
     }
     return jsonify(response)
+
+
+@app.route('/api/results', methods=['POST'])
+def predict_bird():
+    data = request.get_json()
+    
+    bird = data['bird']
+    season = data['season']
+
+    county_state = data['county'] + ',' + data['state']
+    region = cs2r[county_state]
+
+    # encode features
+    X = pd.DataFrame({
+        'name': [bird],
+        'season': [season],
+        'region': [region]
+    })
+    X_encoded = encoder.transform(X)
+    # get prediction
+    pred = model.predict(X_encoded)
+    label = labels[pred[0]]
+
+    response = {
+        'prediction': label
+    }
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
