8ee9dc870c8162f19a27db01ffe56426154dbbc8
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-25 08:15:16 -0400

    initial commit, template app module

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from joblib import load
+
+app = Flask(__name__)
+
+@app.route('/api/birds', methods=['GET'])
+def get_birds():
+    response = {
+        "birds": ['A', 'list', 'of', 'birds']
+    }
+    return jsonify(response)
+
+
+@app.route('/api/seasons', methods=['GET'])
+def get_seasons():
+    response = {
+        "seasons": ['A', 'list', 'of', 'seasons']
+    }
+    return jsonify(response)
+
+
+@app.route('/api/states', methods=['GET'])
+def get_states():
+    response = {
+        "states": ['A', 'list', 'of', 'states']
+    }
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
