ad45cc97ac4acb4a6f94509151efaf1ad36695ef
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-26 09:05:12 -0400

    begin move to lambda deploy, still need credentials

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import pandas as pd
-
+from boto.s3.key import Key
+from boto.s3.connection import S3Connection
 from flask import Flask, jsonify, request
 from joblib import load
 
+BUCKET_NAME = 'ebird-pickle-files'
+LOCAL_PATH = '/tmp/'
+ENCODER_FILE = 'cat_boost.joblib'
+MODEL_FILE = 'rf.joblib'
+BIRDS_FILE = 'birds_list.joblib'
+SEASONS_FILE = 'seasons_list.joblib'
+STATES_FILE = 'states.joblib'
+COUNTIES_FILE = 'state_counties.joblib'
+REGIONS_FILE = 'counties_to_regions.joblib'
+
 app = Flask(__name__)
+
+conn = S3Connection()
+bucket = conn.create_bucket(BUCKET_NAME)
+key_obj = Key(bucket)
 
 encoder = load('utils/cat_boost.joblib')
 model = load('utils/rf.joblib')
 
 birds = load('utils/birds_list.joblib')
 seasons = load('utils/seasons_list.joblib')
-states = load('utils/states.joblib')
+states = load('utils/state_counties.joblib')
 
-state_counties = load('utils/state_counties.joblib')
-cs2r = load('utils/counties_to_regions.joblib')
+state_counties = load('PATH_TO_FILE')
+cs2r = load('PATH_TO_FILE')
 
 labels = {0: "Common", 1:"Uncommon", 2:"Rare"}
 
 @app.route('/api/birds', methods=['GET'])
 def get_birds():
+    key_obj.key = BIRDS_FILE
+    contents = key_obj.get_contents_to_filename(LOCAL_PATH + BIRDS_FILE)
+    birds = joblib.load(LOCAL_PATH + BIRDS_FILE)
     response = {
         "birds": sorted(birds)
     }
