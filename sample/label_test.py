d99c7f5284a073e0420e38730511db08e17e0020
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-11 11:24:51 -0400

    more checking labels on test file

--- sample/label_test.py
+++ sample/label_test.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("sample/labelled_bird_sample.csv")
+
+hummingbirds = df[df['name'] == "Ruby-throated Hummingbird"]
+
+hummingbirds = hummingbirds[hummingbirds['region'] == "Appohzarka"]
+print(hummingbirds[['name', 'season', 'region', 'seas_reg_rare']].head(25))
