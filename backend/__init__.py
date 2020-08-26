2ecf23fc1ef07229cc7faa8cd0a55755aae5bfdb
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-26 08:57:05 -0400

    turn off sqlalchemy track modifications to supress warning - maybe i should have it on though

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,6 +13,7 @@ db_name = os.getenv("DATABASE_NAME")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{pg_user}:{pg_pass}@{db_url}/{db_name}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from backend import routes
